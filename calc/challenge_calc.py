597aff58a015aa5bd0a566d62f749ac45c31a568
mcosnta000 <mconsta000@gmail.com>
2019-06-14 17:49:50 -0400

    Document the classes

--- calc/challenge_calc.py
+++ calc/challenge_calc.py
@@ -1,37 +1,23 @@
 import csv
 
 
-class EncounterDifficultyCalc:
-    difficulty = ["Easy", "Medium", "Hard", "Deadly"]
-
-    def __init__(self, encounter_xp_calc, xp_threshold_calc):
-        self.encounter_xp_calc = encounter_xp_calc
-        self.xp_threshold_calc = xp_threshold_calc
-
-    def calculate_difficulty(self):
-        xp_threshold = self.xp_threshold_calc.calcuate_party_xp_threshold()
-        encounter_xp = \
-            self.encounter_xp_calc.calcualte_adjusted_xp(
-                len(self.xp_threshold_calc.party_levels))
-        encounter_difficulty = "Unknown"
-
-        for i, threshold in reversed(list(enumerate(xp_threshold))):
-            if threshold <= encounter_xp:
-                encounter_difficulty = self.difficulty[i]
-                break
-
-        return encounter_difficulty
-
-
 class EncounterXPCalc:
+    """Calculate the adjusted encounter xp"""
+
     def __init__(self):
         self.encounter_xp = []
 
-    def add_encounter_xp(self, encounter_xp):
+    def add_encounter_xp(self, encounter_xp: int):
+        """Add difficulty class as an integer for each member of the
+        opposing party"""
+
         self.encounter_xp.append(encounter_xp)
         pass
 
-    def calcualte_adjusted_xp(self, party_size=5):
+    def calcualte_adjusted_xp(self, party_size: int=5) -> int:
+        """Calculates the adjusted xp based on the challenging party 
+        sizze"""
+
         encounter_multiplier = 0
         adjusted_xp = 0
         multiplier = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0]
@@ -66,11 +52,16 @@ class EncounterXPCalc:
 
 
 class XPThresholdCalc:
+    """Calculate the party xp threshold"""
+
     def __init__(self):
+        """Intitilize member variables and load xp threshold table"""
+
         self.party_levels = []
         self.party_xp_theshold = []
         self.thresholds = {}
 
+        # TODO: make thresholds map a class variable
         with open("resource/xp_threshold.csv") as thresholds:
             reader = csv.reader(thresholds, delimiter=',')
             for row in reader:
@@ -79,11 +70,15 @@ class XPThresholdCalc:
 
         pass
 
-    def add_party_level(self, character_level):
+    def add_party_level(self, character_level: int):
+        """Add each party member as an integer value"""
+
         self.party_levels.append(character_level)
         pass
 
-    def calcuate_party_xp_threshold(self):
+    def calcuate_party_xp_threshold(self) -> [int]:
+        """Calculate the party xp threshold as an array of 4 integers"""
+
         self.party_xp_threshold = [0] * 4
 
         for level in self.party_levels:
@@ -91,3 +86,36 @@ class XPThresholdCalc:
                 x + y for x, y in zip(self.party_xp_threshold, self.thresholds[level])]
 
         return self.party_xp_threshold
+
+
+class EncounterDifficultyCalc:
+    """Calculate the difficulty of an encounter"""
+
+    difficulty = ["Easy", "Medium", "Hard", "Deadly"]
+
+    def __init__(self, encounter_xp_calc: EncounterXPCalc, 
+        xp_threshold_calc: XPThresholdCalc):
+        """Construct the EncounterDifficultyCalc with EncounterXPCalc 
+        and XPThresholdCalc.  This class uses the calculations 
+        provided by the passed classes to determine the difficulty of
+        the encounter"""
+
+        self.encounter_xp_calc = encounter_xp_calc
+        self.xp_threshold_calc = xp_threshold_calc
+
+    def calculate_difficulty(self) -> str:
+        """Determine encounter difficulty using object passed during
+        instantiation"""
+
+        xp_threshold = self.xp_threshold_calc.calcuate_party_xp_threshold()
+        encounter_xp = \
+            self.encounter_xp_calc.calcualte_adjusted_xp(
+                len(self.xp_threshold_calc.party_levels))
+        encounter_difficulty = "Unknown"
+
+        for i, threshold in reversed(list(enumerate(xp_threshold))):
+            if threshold <= encounter_xp:
+                encounter_difficulty = self.difficulty[i]
+                break
+
+        return encounter_difficulty

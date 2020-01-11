dd01e72539532d2917ba39db3e25cf6f472c243a
mcosnta000 <mconsta000@gmail.com>
2020-01-11 14:50:07 -0500

    Create packaging for module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='challenge-calc',
+    version='master',
+    packages=['challenge',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)

af5d57329056c97136e915346ffd990791b4b71c
mcosnta000 <mconsta000@gmail.com>
2020-01-17 15:59:34 -0500

    Follow packaging tutorial pattern

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='challenge-calc',
     version='0.1.0',
     author="mconsta000",
     packages=setuptools.find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.6',
 )
